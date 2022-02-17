89d0774e4976dc6b239b873d1e0da0c1f2becd96
motlubor <motlubor2591997@gmail.com>
2022-02-17 17:43:41 +0600

    calculate foodCost, rentCost & clothesCost with get sum

--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
-console.log('everything is okay');
+
+
+document.getElementById('calculate-cost').addEventListener('click', function () {
+
+    // get all input value from all type of input fields 
+
+    const foodCostInput = document.getElementById('food-cost');
+    const foodCost = parseFloat(foodCostInput.value);
+
+    const rentCostInput = document.getElementById('rent-cost');
+    const rentCost = parseFloat(rentCostInput.value);
+
+    const clothesCostInput = document.getElementById('clothes-cost');
+    const clothesCost = parseFloat(clothesCostInput.value);
+
+    // sum of all input field value 
+
+    const totalCost = foodCost + rentCost + clothesCost;
+
+
+    //  catch total expences field
+    const totalExpences = document.getElementById('total-expences');
+
+    totalExpences.innerText = totalCost;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
